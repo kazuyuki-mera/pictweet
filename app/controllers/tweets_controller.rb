50797a110b1bcbd09ed7319923dd08c3038feff8
kazuyuki_mera <mera.kazuyuki@techbase.jp>
2020-05-04 00:40:09 +0900

    投稿画面作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,20 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all
   end
 
+  def new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+
+  private
+  # ストロングパラメーター
+  # ストロングパラメーターとは、指定したキーを持つパラメーターのみを受け取るようにするものです。
+  # もし仮に悪意のあるユーザーが、不正な情報を送信しようとしたときに、ストロングパラメーターを設定しておくと、不正な情報を受け取らずにすみます。
+  def tweet_params
+    params.permit(:name, :image, :text)
+  end
+
 end
