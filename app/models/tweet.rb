6de08588c6b31a78b56320620bf2907d9448d48f
kazuyuki_mera <mera.kazuyuki@techbase.jp>
2020-05-04 16:07:25 +0900

    コメント機能の追加

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,4 @@
 class Tweet < ApplicationRecord
-  belongs_to :user
+  belongs_to :user    # usersテーブルとのアソシエーション
+  has_many :comments  # commentsテーブルとのアソシエーション
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  root   'tweets#index'                       #ルートパスの指定
-  get    'tweets'      =>  'tweets#index'     #ツイート一覧画面
-  get    'tweets/new'  =>  'tweets#new'       #ツイート投稿画面
-  post   'tweets'      =>  'tweets#create'    #ツイート投稿機能
-  delete 'tweets/:id'  =>  'tweets#destroy'   #ツイート削除機能
-  patch  'tweets/:id'  =>  'tweets#update'    #ツイート編集機能
-  get    'tweets/:id/edit'  => 'tweets#edit'  #ツイート編集画面
-  get    'tweets/:id'  =>  'tweets#show'      #ツイート詳細画面
-  get    'users/:id'   =>  'users#show'       #マイページへのルーティング
+  root   'tweets#index'                 # ルートパスの指定
+  resources :tweets                     # tweets_controllerに対してのresourcesメソッド
+  resources :tweets do                  # comments_controllerに対してのresourcesメソッド
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]       # users_controllerに対してのresourcesメソッド
 end
+
+  # get    'tweets'      =>  'tweets#index'     #ツイート一覧画面
+  # get    'tweets/new'  =>  'tweets#new'       #ツイート投稿画面
+  # post   'tweets'      =>  'tweets#create'    #ツイート投稿機能
+  # delete 'tweets/:id'  =>  'tweets#destroy'   #ツイート削除機能
+  # patch  'tweets/:id'  =>  'tweets#update'    #ツイート編集機能
+  # get    'tweets/:id/edit'  => 'tweets#edit'  #ツイート編集画面
+  # get    'tweets/:id'  =>  'tweets#show'      #ツイート詳細画面
+  # get    'users/:id'   =>  'users#show'       #マイページへのルーティング
