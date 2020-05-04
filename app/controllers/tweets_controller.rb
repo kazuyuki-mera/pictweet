d36f9c89b2ff4cf4229645fb0529342ec4b83242
kazuyuki_mera <mera.kazuyuki@techbase.jp>
2020-05-04 12:06:43 +0900

    ログイン機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,8 @@
 class TweetsController < ApplicationController
 
+   # 全てのアクションが実行される前に実行されるアクション
+  before_action :move_to_index, except: :index
+
   def index
     # ツイートを最新順にソート
     @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
@@ -19,6 +22,11 @@ class TweetsController < ApplicationController
   # もし仮に悪意のあるユーザーが、不正な情報を送信しようとしたときに、ストロングパラメーターを設定しておくと、不正な情報を受け取らずにすみます。
   def tweet_params
     params.permit(:name, :image, :text)
+  end
+
+  # 未ログインの場合、indexアクションを実行する
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
